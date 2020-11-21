2fa32f4c2d7001bad0f86aabcded53fa5e4d2b78
Mr. Robot <t0thkr1s@icloud.com>
2020-11-21 19:07:02 +0100

    add package placeholder & time delay
    Time delay is required on some Android devices.

--- scripts/root_check_bypass.py
+++ scripts/root_check_bypass.py
@@ -5,7 +5,7 @@
 import frida
 import sys
 
-package_name = "infosecadventures.fridademo"
+package_name = "INSERT_PACKAGE_HERE"
 
 script = """
 Java.perform(function() {
@@ -372,7 +372,7 @@ Java.perform(function() {
 
 try:
     print("[ * ] Looking for app: " + package_name)
-    device = frida.get_usb_device()
+    device = frida.get_usb_device(10)
     print("[ * ] Launching app...")
     pid = device.spawn([package_name])
     device.resume(pid)
